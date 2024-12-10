d67638b96ac5f359bf4dfc5e1e423fbea68173be
DreamingLri <3507415455@qq.com>
2024-12-10 10:33:20 +0800

    ✨ 将路由注册分为需要验证和无需验证

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"suask/internal/controller/hello"
 	"suask/internal/controller/register"
+	"suask/internal/controller/user"
 	"suask/internal/service"
 
 	"github.com/gogf/gf/v2/frame/g"
@@ -18,14 +19,30 @@ var (
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
+
+			gfToken, err := LoginToken()
+			if err != nil {
+				return err
+			}
+
 			s.Group("/", func(group *ghttp.RouterGroup) {
 				group.Middleware(
 					ghttp.MiddlewareHandlerResponse,
 					service.Middleware().CORS)
 				group.Bind(
-					hello.NewV1(),
 					register.Register.Register,
+					user.User.GetUserInfoById,
 				)
+				group.Group("/", func(group *ghttp.RouterGroup) {
+					err := gfToken.Middleware(ctx, group)
+					if err != nil {
+						panic(err)
+					}
+					group.Bind(
+						hello.NewV1(),
+						user.User.Info,
+					)
+				})
 			})
 			s.Run()
 			return nil
