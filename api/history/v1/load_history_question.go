4bba9df9a54e019aee1271292ef4954653a0ade7
Bubnle <1270904398@qq.com>
2024-12-12 01:03:40 +0800

    change2

--- api/history/v1/load_history_question.go
+++ api/history/v1/load_history_question.go
@@ -2,14 +2,15 @@ package v1
 
 import (
 	"suask/internal/model"
+
 	"github.com/gogf/gf/v2/frame/g"
 )
 
-type LoadHistoryQuestionReq struct{
-	go.Meta `path:"/history method:"GET summary:"获取历史提问请求"`
-	UserId   int `v:"required|min:1" json:"user_id" dc:"发起获取历史提问请求的UserId"`
+type LoadHistoryQuestionReq struct {
+	g.Meta `path:"/history" method:"GET summary:"获取历史提问请求"`
+	UserId int `v:"required|min:1" json:"user_id" dc:"发起获取历史提问请求的UserId"`
 }
 
-type LoadHistoryQuestionRes struct{
+type LoadHistoryQuestionRes struct {
 	HistoryQuestionList []model.PublicQuestion `json:"question_list" dc:"后端返回的历史提问列表"`
 }
