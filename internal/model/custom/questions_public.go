3cf1de47cfc4c5ec5fdc38dc58aa570d0fe07828
Jacko <1730072668@qq.com>
2024-12-19 20:10:40 +0800

    修复获取问题时没办法过滤favorites的user_id的问题

--- internal/model/custom/questions_public.go
+++ internal/model/custom/questions_public.go
@@ -12,31 +12,28 @@ type PublicQuestions struct {
 	Contents  string      `json:"contents"  orm:"contents"    description:"问题内容"`  // 问题内容
 	CreatedAt *gtime.Time `json:"created_at" orm:"created_at"  description:"创建时间"` // 创建时间
 	Views     int         `json:"views"     orm:"views"       description:"浏览量"`   // 浏览量
-	Upvotes   int         `json:"upvotes"   orm:"upvotes"     description:"点赞量"`   // 点赞量
 
-	Images      []*Image          `json:"images"  orm:"with:question_id=id"`       // 图片附件
-	IsFavorited []*UserFavorites  `json:"is_favorited" orm:"where:question_id=id"` // 是否收藏
-	Answers     []*QuestionAnwers `json:"answers"   orm:"with:question_id=id"`     // 回答
+	Images  []*Image          `json:"images"  orm:"with:question_id=id"`   // 图片附件(暂时没有图片，大概率有bug)
+	Answers []*QuestionAnwers `json:"answers"   orm:"with:question_id=id"` // 回答 (暂时没有回答，大概率有bug)
 }
 
 type Image struct {
 	g.Meta `orm:"table:attachments"`
-	Type   string `json:"type"   orm:"type"         description:"附件类型（目前仅支持图片）"`
-	FileID int    `json:"fileId" orm:"file_id"      description:"文件ID"` // 文件ID
-}
-
-type UserFavorites struct {
-	g.Meta `orm:"table:favorites"`
-	UserID int `json:"userId"     orm:"user_id"     description:"用户ID"` // 用户ID
+	FileID int `json:"file_id" orm:"file_id"      description:"文件ID"` // 文件ID
 }
 
 type QuestionAnwers struct {
 	g.Meta `orm:"table:answers"`
-	Id     int          `json:"id"        orm:"id"          description:"回答ID"`    // 回答ID
-	Users  *AnswerUsers `json:"users"     orm:"with:user_id=id" description:"回答者"` // 回答者
+	Id     int          `orm:"id"          description:"回答ID"`    // 回答ID
+	Users  *AnswerUsers `orm:"with:user_id=id" description:"回答者"` // 回答者
 }
 
 type AnswerUsers struct {
 	g.Meta `orm:"table:users"`
-	Avatar int `json:"avatar" orm:"avatar_file_id" description:"回答者头像文件ID"` // 回答者头像文件ID
+	Avatar int `orm:"avatar_file_id" description:"回答者头像文件ID"` // 回答者头像文件ID
+}
+
+type MyFavorites struct {
+	g.Meta     `orm:"table:favorites"`
+	QuestionId int `json:"question_id" orm:"question_id" description:"问题ID"` // 问题ID
 }
