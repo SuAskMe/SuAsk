24ddf572a7314239639c7bc67f69103fafb9cb7a
Jacko <1730072668@qq.com>
2025-03-09 14:23:31 +0800

    feat(api): 添加心跳请求结构体 feat(internal/cmd): 添加心跳请求处理 feat(internal/cmd): 心跳请求加入必登录路径 feat(internal/controller/login): 添加心跳请求处理逻辑 feat(internal/logic/login): 添加心跳请求服务接口 docs(internal/service): 添加心跳请求接口注释

--- internal/controller/login/login.go
+++ internal/controller/login/login.go
@@ -2,21 +2,29 @@ package login
 
 import (
 	"context"
-	"suask/api/login/v1"
+	v1 "suask/api/login/v1"
+	"suask/internal/consts"
+
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 type cLogin struct{}
 
 var Login cLogin
 
-func (c *cLogin) Login(ctx context.Context, req v1.LoginReq) (res v1.LoginRes, err error) {
+func (c *cLogin) Login(ctx context.Context, req *v1.LoginReq) (res *v1.LoginRes, err error) {
 	return
 }
 
-func (c *cLogin) Logout(ctx context.Context, req v1.LogoutReq) (res v1.LogoutRes, err error) {
+func (c *cLogin) Logout(ctx context.Context, req *v1.LogoutReq) (res *v1.LogoutRes, err error) {
 	return
 }
 
-func (c *cLogin) RefreshToken(ctx context.Context, req v1.RefreshTokenReq) (res v1.RefreshTokenRes, err error) {
+func (c *cLogin) HeartBeats(ctx context.Context, req *v1.HeartBeatsReq) (res *v1.HeartBeatsRes, err error) {
+	userId := gconv.Int(ctx.Value(consts.CtxId))
+	return &v1.HeartBeatsRes{UserId: userId}, nil
+}
+
+func (c *cLogin) RefreshToken(ctx context.Context, req *v1.RefreshTokenReq) (res *v1.RefreshTokenRes, err error) {
 	return
 }
